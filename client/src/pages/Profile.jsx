5cc5a08f107bfbac6b9460609824ba857fbac73f
GarvThanvi <gtuhm2004@gmail.com>
2024-06-18 21:34:33 +0530

    complete listing delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -142,6 +142,24 @@ export default function Profile() {
       showListingsError(true);
     }
   };
+
+  const handleDeleteListings = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -229,13 +247,12 @@ export default function Profile() {
         {showListingsError ? "Error showing listings" : ""}
       </p>
 
-      <div className="flex flex-col gap-4">
-        <h1 className="text-center my-7 text-2xl font-semibold">
-          Your Listings
-        </h1>
-        {userListings &&
-          userListings.length > 0 &&
-          userListings.map((listing) => (
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center my-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
             <div
               className="border rounded-lg flex items-center p-3 justify-between gap-4"
               key={listing._id}
@@ -255,12 +272,18 @@ export default function Profile() {
               </Link>
 
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  onClick={() => handleDeleteListings(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
           ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
